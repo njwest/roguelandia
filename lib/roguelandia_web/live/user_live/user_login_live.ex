b7f2b424f830c6d2d9e4873c3461e1415e17af2c
Nick West <njwest9@gmail.com>
2024-06-20 18:03:58 -0400

    more light/dark

--- lib/roguelandia_web/live/user_live/user_login_live.ex
+++ lib/roguelandia_web/live/user_live/user_login_live.ex
@@ -21,7 +21,7 @@ defmodule RoguelandiaWeb.UserLive.UserLoginLive do
 
         <:actions>
           <.input field={@form[:remember_me]} type="checkbox" label="Keep me logged in" />
-          <.link href={~p"/users/reset_password"} class="text-sm font-semibold">
+          <.link href={~p"/users/reset_password"} class=" font-semibold">
             Forgot your password?
           </.link>
         </:actions>
