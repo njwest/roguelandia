6b076e52b4f999b1dbabf618c9f6ea5cea321377
Nick West <njwest9@gmail.com>
2024-06-23 11:25:45 -0400

    new notifs mig

--- priv/repo/migrations/20240620210937_add_notifs.exs
+++ priv/repo/migrations/20240620210937_add_notifs.exs
@@ -1,25 +1,17 @@
 defmodule Roguelandia.Repo.Migrations.NotifTypesAndNotifs do
   use Ecto.Migration
 
-  def change do
-    create table(:notification_types) do
-      add :type, :string
-      add :content, :string
-
-      timestamps()
-    end
-
+  def change() do
     create table(:notifications) do
       add :actor_id, :integer
       add :entity_id, :integer
-      add :status, :string
+      add :entity_type, :string
       add :user_id, references(:users, on_delete: :nothing)
-      add :notification_type_id, references(:notification_types, on_delete: :nothing)
+      add :read_at, :naive_datetime
 
       timestamps()
     end
 
     create index(:notifications, [:user_id])
-    create index(:notifications, [:notification_type_id])
   end
 end
